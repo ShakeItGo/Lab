7d5a5df6628a7dbe69489603280580c3a3862dce
ShakeItGo <artur.potar@gmail.com>
2022-09-03 14:09:30 +0300

    second commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
